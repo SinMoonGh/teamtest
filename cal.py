c6f36857c2422c67faa1346dc451bac31fb87c6c
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-03-11 22:11:34 +0900

    'calculator'

--- cal.py
+++ cal.py
@@ -4,11 +4,13 @@
 # 나누기
 def divide(a,b):
     return a/b
+
+# 곱하기
+def doble(a,b):
+    return a*b
     
 # 뺄셈 계산
-def minus():
-  num1 = int(input("첫번째 숫자 입력: "))
-  num2 = int(input("두번째 숫자 입력: "))
+def minus(num1, num2):
   result = num1 - num2
   return result
 
--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import cal
 while True:
 
     # 계산할 값을 입력
-    a = input("첫번째 값을 입력하세요.")
+    a = int(input("첫번째 값을 입력하세요."))
     if a == 'q':
         break
-    b = input("두 번째 값을 입력하세요.")
+    b = int(input("두 번째 값을 입력하세요."))
     mark = input("입력하세요 : [+, -, /, *]")
 
     # 사칙연산을 골라야 함.
     if mark == '+':
-        pass
+        print("결과값 : {}".format(cal.add(a, b)))
     elif mark == '-':
-        pass
+        print("결과값 : {}".format(cal.minus(a, b)))
     elif mark == '*':
-        pass
+        print("결과값 : {}".format(cal.doble(a, b)))
     elif mark == '/':
-        pass
+        print("결과값 : {}".format(cal.divide(a, b)))
 
-    print("결과값 : {}")
+    
 
     # 변경확인
     
